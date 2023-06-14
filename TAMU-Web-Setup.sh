ac198005770bf9d7a1861d0f123dd7bd24e1031c
Michael Nichols <128824999+mnicholstamu@users.noreply.github.com>
2023-06-14 10:17:24 -0500

    updated with brew doctor

--- TAMU-Web-Setup.sh
+++ TAMU-Web-Setup.sh
@@ -11,6 +11,14 @@ pretty_print "Setting up TMAU Web Environment..."
 if ! command -v brew &>/dev/null; then
   pretty_print "Installing Homebrew, an OSX package manager, follow the instructions..." 
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    if ! grep -qs "recommended by brew doctor" ~/.zshrc; then
+      pretty_print "Put Homebrew location earlier in PATH ..."
+      printf '\n# recommended by brew doctor\n' >> ~/.zshrc
+      printf 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
+      export PATH="/usr/local/bin:$PATH"
+    fi
+
   else
     pretty_print "You already have Homebrew installed...good job!"
 fi
